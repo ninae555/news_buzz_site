771a6925eec691387361300b8ed3ab1f4bd46bfb
Meet Daxini <meetdaxini10@gmail.com>
2023-11-02 18:32:19 -0400

    added nlp integrations for categorizing articles

--- news_buzz/articles/admin.py
+++ news_buzz/articles/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from import_export.admin import ImportExportMixin 
-from news_buzz.articles.models import Article, Publisher, ArticleSent, ArticleTimeSpent, Reaction, ReadEntireArticleClick
+from news_buzz.articles.models import Article, Publisher, ArticleSent, ArticleTimeSpent, Reaction, ReadEntireArticleClick, Category
 
 @admin.register(Publisher)
 class PublisherAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ["id", "domain", "pc1", "created", "modified"]
 
+
+@admin.register(Category)
+class CategoryAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ["id", "name", "created", "modified"]
+    search_fields = ["name", "id"]
 
 @admin.register(Article)
 class ArticleAdmin(ImportExportMixin, admin.ModelAdmin):
