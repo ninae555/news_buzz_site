c361d3d1a9693d3252344643c3a22c4c2960c4b5
Meet Daxini <meetdaxini10@gmail.com>
2024-03-10 13:00:26 -0400

    updated admin config for csv export of reactions and clicks

--- news_buzz/articles/admin.py
+++ news_buzz/articles/admin.py
@@ -1,10 +1,49 @@
 from django.contrib import admin
-from import_export.admin import ImportExportMixin 
-from news_buzz.articles.models import Article, Publisher, ArticleSent, ArticleTimeSpent, Reaction, ReadEntireArticleClick, Category
+from import_export.admin import ImportExportMixin
+from news_buzz.articles.models import (
+    Article,
+    Publisher,
+    ArticleSent,
+    Reaction,
+    ReadEntireArticleClick,
+    Category,
+)
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+class ReactionResource(resources.ModelResource):
+
+    class Meta:
+        model = Reaction
+        fields = (
+            "id",
+            "created",
+            "type",
+            "participant",
+            "participant__participant_id",
+            "article",
+            "article__title",
+            "categories",
+        )
+
+
+class ReadEntireArticleClickResource(resources.ModelResource):
+    class Meta:
+        model = ReadEntireArticleClick
+        fields = (
+            "id",
+            "participant",
+            "session",
+            "article__title",
+            "categories",
+        )
+
 
 @admin.register(Publisher)
 class PublisherAdmin(ImportExportMixin, admin.ModelAdmin):
-    list_display = ["id", "domain", "pc1", "created", "modified"]
+    list_display = ["id", "domain", "name", "pc1", "created", "modified"]
+    search_fields = ["domain", "id", "name"]
 
 
 @admin.register(Category)
@@ -12,19 +51,25 @@ class CategoryAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ["id", "name", "created", "modified"]
     search_fields = ["name", "id"]
 
+
 @admin.register(Article)
 class ArticleAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ["id", "published_at", "publisher", "title", "url", "created", "modified"]
     search_fields = ["title", "id"]
 
+
 @admin.register(ReadEntireArticleClick)
-class ReadEntireArticleClickAdmin(ImportExportMixin, admin.ModelAdmin):
+class ReadEntireArticleClickAdmin(ImportExportModelAdmin):
+    resource_class = ReadEntireArticleClickResource
     list_display = ["id", "participant", "session", "article"]
+
 
 @admin.register(ArticleSent)
 class ArticleSentAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ["id", "session", "article"]
 
+
 @admin.register(Reaction)
 class ReactionAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ["id", "participant", "article", "type"]
+    resource_class = ReactionResource
